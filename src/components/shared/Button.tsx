35e56570d783aeb4efc831e66c80d57eac78a8b5
Rahul Mijar <rahul.mijar@trunary.com>
2024-02-23 23:55:25 +0545

    feat: add footer

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -8,7 +8,7 @@ const Button = ({ text, className = "", onClick }: ButtonProp) => {
     <button
       onClick={onClick}
       type="button"
-      className={`${className} text-white bg-blue-600 font-medium rounded-lg text-sm px-4 py-2 text-center shadow-lg hover:translate-y-1 hover:shadow-none`}
+      className={`${className} text-white bg-gradient-to-r from-blue-500 to-purple-600 font-medium rounded-lg text-sm px-4 py-2 text-center shadow-lg hover:translate-y-1 hover:shadow-none`}
     >
       {text}
     </button>
